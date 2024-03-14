0e3306111a79322d7987e65ce24bac13014709c0
hassanzali <hassanzali622@gmail.com>
2024-03-14 23:05:29 +0100

    modhle_importation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arguments = sys.argv[1:]
+
     result = 0
-    for i in arguments:
-        result += int(i)
-    print(result)
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
